eb24593b2648288f93ca23df47442b641c95430b
seungchae <s2125@e-mirim.hs.kr>
2022-04-01 02:43:41 +0900

    코딩테스트Lv1_핸드폰 번호 가리기(substring 활용)

--- src/Lv1/Lv1_05.java
+++ src/Lv1/Lv1_05.java
@@ -1,14 +1,13 @@
 package Lv1;
 
 public class Lv1_05 {
-	public String solution(String phone_number) {
-		String[] arr = phone_number.split("");
-        String answer="";
+    public String solution(String phone_number) { 
+        String answer = ""; 
+        for( int i = 0; i < phone_number.length() - 4; i++ ) { 
+            answer += "*"; 
+        } 
         
-		for(int i=0; i<arr.length; i++) {
-            if(i<arr.length-4) answer+='*';
-            else answer+=arr[i];
-        }
+        answer += phone_number.substring(phone_number.length()-4, phone_number.length()); 
         return answer;
-    }
+    } 
 }
