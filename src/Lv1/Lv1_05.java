b2a3c228f9ec29d53b048b274194a55620d2898e
seungchae <s2125@e-mirim.hs.kr>
2022-04-01 01:37:04 +0900

    코딩테스트Lv1_핸드폰 번호 가리기(split사용)

--- src/Lv1/Lv1_05.java
+++ src/Lv1/Lv1_05.java
@@ -1,16 +1,14 @@
 package Lv1;
-import java.util.*;
 
 public class Lv1_05 {
 	public String solution(String phone_number) {
-		char[] answer1 = new char[phone_number.length()];
-		char[] answer2 = phone_number.toCharArray();
+		String[] arr = phone_number.split("");
+        String answer="";
         
-		for(int i=0; i<answer1.length; i++) {
-            if(i<answer1.length-4) answer1[i]='*';
-            else answer1[i]=answer2[i];
+		for(int i=0; i<arr.length; i++) {
+            if(i<arr.length-4) answer+='*';
+            else answer+=arr[i];
         }
-        String answer=new String(answer1);
         return answer;
     }
 }
