dcfd4117f63cf2d5e707880cbc2c6105e0972b81
seungchae <s2125@e-mirim.hs.kr>
2022-04-11 23:37:27 +0900

    정수 제곱근 판별

--- src/Lv1/Lv1_09.java
+++ src/Lv1/Lv1_09.java
@@ -1,14 +1,13 @@
 package Lv1;
 
 public class Lv1_09 {
-	public int solution(int n) {
-        int answer = 0;
-        for(int i=1; i<=n; i++){
-            if(n%i==1){
-                answer=i;
-                break;
-            }
-        }
-        return answer;
-    }
+	public class Solution {
+	    
+	    public long solution(long n) {
+	        for(long i=1; i<=n; i++){
+	            if(n/i==i) return (i+1)*(i+1);
+	        }
+	        return -1;
+	    }
+	}
 }
