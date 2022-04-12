4b922808731a4a4285859dfde33479ba71c1cb0b
seungchae <s2125@e-mirim.hs.kr>
2022-04-13 00:57:07 +0900

    약수의 개수와 덧셈(해결)

--- src/Lv1/Lv1_12.java
+++ src/Lv1/Lv1_12.java
@@ -3,8 +3,8 @@ package Lv1;
 public class Lv1_12 {
     public int solution(int left, int right) {
         int answer = 0;
-        int cnt=0;
         for(int i=left; i<=right; i++){
+            int cnt=0;
             for(int j=1; j<=i; j++){
                 if(i%j==0)cnt++;
             }
