b0c9aaa647ba091fd12510f12b4198c205c0e415
seungchae <s2125@e-mirim.hs.kr>
2022-04-13 01:01:00 +0900

    정수 제곱근 판별(해결)

--- src/Lv1/Lv1_06.java
+++ src/Lv1/Lv1_06.java
@@ -1,15 +1,10 @@
 package Lv1;
 
 public class Lv1_06 {
-    public long solution(long n) {
-        long answer = 0;
-        for(int i=1; i<=n; i++){
-            if(n/i==i){
-                answer=(i+1)*(i+1);
-                break;
-            }
-        }
-        if(answer==0) answer=-1;
-        return answer;
-    }
+	 public long solution(long n) {
+		 for(long i=1; i<=n; i++){
+			 if(n/i==i) return (i+1)*(i+1);
+		 }
+		 return -1;
+	 }
 }
