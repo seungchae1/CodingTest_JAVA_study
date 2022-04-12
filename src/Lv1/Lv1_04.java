2835372eeb51fd0335f988a71a231e11c25e796a
seungchae <s2125@e-mirim.hs.kr>
2022-04-12 21:49:34 +0900

    완주하지 못한 선수(적확성 50/효율성 0)

--- src/Lv1/Lv1_04.java
+++ src/Lv1/Lv1_04.java
@@ -1,20 +1,22 @@
 package Lv1;
 
-public class Lv1_04 {
-	public String solution(String[] participant, String[] completion) {
+public class Lv1_04 { 
+    public String solution(String[] participant, String[] completion) {
         String answer = "";
+        int index=0;
+        int cnt;
         for(int i=0; i<participant.length; i++){
+            cnt=0;
             for(int j=0; j<completion.length; j++){
-                   if(participant[i]==completion[j]){
-                       completion[j]=""; 
-                       participant[i]="";    
-                   }
+                if(participant[i].equals(completion[j])){
+                    completion[j]=""; 
+                    participant[i]="";
+                    cnt++;
+                }
             }
+            if(cnt==0)index=i;
         }
-        for(int i=0; i<participant.length; i++){
-            if(participant[i]!="") answer=participant[i];
-        }
-        
+        answer=participant[index];
         return answer;
     }
 }
