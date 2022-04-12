015edc81c10d10f4542a36271b72463e26af62e8
seungchae <s2125@e-mirim.hs.kr>
2022-04-13 01:06:16 +0900

    자릿수 더하기(해결)

--- src/Lv1/Lv1_13.java
+++ src/Lv1/Lv1_13.java
@@ -1,20 +1,12 @@
 package Lv1;
 
 public class Lv1_13 {
-	public int solution(int n) {
+    public int solution(int n) {
         int answer = 0;
-        int m=n;
-        int cnt=0;
-        while(m>10){
-            m/=10;
-            cnt++;
+        for(int i=1; n>0; i++){
+            answer+=n%10;
+            n/=10;
         }
-        int flag=1;
-        for(int i=cnt; i>=1; i--){
-            flag*=10;
-            answer+=(n-answer*flag)/flag;
-        } 
-            
         return answer;
     }
 }
