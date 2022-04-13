8bb9eb2fa77c71f65d4e76c2a1989fab7b9aa0b0
seungchae <s2125@e-mirim.hs.kr>
2022-04-14 08:40:38 +0900

    하샤드 수(미해결)

---
+++ src/Lv1/Lv1_18.java
@@ -0,0 +1,22 @@
+package Lv1;
+
+public class Lv1_18 {
+    public boolean solution(int x) {
+        boolean answer = false;
+        String a=""+x;
+        int[] b=new int[a.length()];
+        int y=x;
+        int i=0;
+        while(y>=10){
+            b[i]=y%10;
+            y/=10;
+            i++;
+        }
+        y=0;
+        for(i=0; i<b.length; i++){
+            y+=b[i];
+        }
+        if(x%y==0)answer=true;
+        return answer;
+    }
+}
