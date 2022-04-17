a938fd78b44d6a9c7f6e6cf6409b337e72e6b599
seungchae <s2125@e-mirim.hs.kr>
2022-04-18 02:00:38 +0900

    두 정수 사이의 합

---
+++ src/Lv1/Lv1_24.java
@@ -0,0 +1,12 @@
+package Lv1;
+
+public class Lv1_24 {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a>b)
+            for(int i=b; i<=a; i++) answer+=i;
+        else 
+            for(int i=a; i<=b; i++) answer+=i;
+        return answer;
+    }
+}
