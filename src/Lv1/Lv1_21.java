0e1be48c2bb77a4826cb91ee715c81c715f611b9
seungchae <s2125@e-mirim.hs.kr>
2022-04-17 21:55:18 +0900

    부족한 금액 계산하기(음수 해결)

--- src/Lv1/Lv1_21.java
+++ src/Lv1/Lv1_21.java
@@ -3,7 +3,7 @@ package Lv1;
 public class Lv1_21 {
     public long solution(int price, int money, int count) {
     	long answer = 0;
-        long sum=0;
+        int sum=0;
         for(int i=1; i<=count; i++){
             sum+=price*i;
         }
