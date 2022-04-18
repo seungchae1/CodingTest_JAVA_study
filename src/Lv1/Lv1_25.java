38bd49e23e2c299dd5cda6073d6cef04cbb94805
seungchae <s2125@e-mirim.hs.kr>
2022-04-18 14:57:45 +0900

    나누어 떨어지는 숫자 배열(미해결)

--- src/Lv1/Lv1_25.java
+++ src/Lv1/Lv1_25.java
@@ -5,12 +5,27 @@ public class Lv1_25 {
         int cnt=0;
         for(int i=0; i<arr.length; i++)
             if(arr[i]%divisor==0)cnt++;
-        int[] answer = new int[cnt];
-        int j=0;
-        for(int i=0; j<cnt; i++){
-            if(arr[i]%divisor==0)answer[j]=arr[i];
-            j++;
-        } 
-        return answer;
+        if(cnt==0){
+            int[] answer={-1};
+            return answer;
+        }
+        else{
+            int[] answer = new int[cnt];
+            int j=0;
+            for(int i=0; j<answer.length; i++){
+                if(arr[i]%divisor==0)answer[j]=arr[i];
+                j++;
+            }
+            for(int i=0; i<answer.length-1; i++){
+                for(int y=1; y<answer.length; y++){
+                    if(answer[i]>answer[y]){
+                    int flag=answer[i];
+                    answer[i]=answer[y];
+                    answer[y]=flag;
+                    }
+                }
+            }
+             return answer;
+        }
     }
 }
