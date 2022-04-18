ce9d25bc678b39d3964cc226f55e341f7abf2c98
seungchae <s2125@e-mirim.hs.kr>
2022-04-18 18:06:03 +0900

    오름차순 정렬(Arrays패키지의 sort메소드)

--- src/Lv1/Lv1_25.java
+++ src/Lv1/Lv1_25.java
@@ -1,31 +1,19 @@
 package Lv1;
-
+import java.util.Arrays;
 public class Lv1_25 {
     public int[] solution(int[] arr, int divisor) {
+        int[] answer={-1};
         int cnt=0;
         for(int i=0; i<arr.length; i++)
             if(arr[i]%divisor==0)cnt++;
-        if(cnt==0){
-            int[] answer={-1};
-            return answer;
+        if(cnt==0) return answer;
+        answer = new int[cnt];
+        int j=0;
+        for(int i=0; j<cnt; i++){
+            if(arr[i]%divisor==0)answer[j]=arr[i];
+            j++;
         }
-        else{
-            int[] answer = new int[cnt];
-            int j=0;
-            for(int i=0; j<answer.length; i++){
-                if(arr[i]%divisor==0)answer[j]=arr[i];
-                j++;
-            }
-            for(int i=0; i<answer.length-1; i++){
-                for(int y=1; y<answer.length; y++){
-                    if(answer[i]>answer[y]){
-                    int flag=answer[i];
-                    answer[i]=answer[y];
-                    answer[y]=flag;
-                    }
-                }
-            }
-             return answer;
-        }
+        Arrays.sort(answer);
+        return answer;
     }
 }
