74ca9a3199d04dcbb5e2086e2ae36c03ed384f18
seungchae <s2125@e-mirim.hs.kr>
2022-04-19 11:29:29 +0900

    모의고사(해결)

--- src/Lv1/Lv1_28.java
+++ src/Lv1/Lv1_28.java
@@ -2,37 +2,38 @@ package Lv1;
 import java.util.Arrays;
 public class Lv1_28 {
     public int[] solution(int[] answers) {
-        int[] sp1={1,2,3,4,5};
+		int[] sp1={1,2,3,4,5};
         int[] sp2={2,1,2,3,2,4,2,5};
         int[] sp3={3,3,1,1,2,2,4,4,5,5};
         int sp1len=sp1.length;
         int sp2len=sp2.length;
         int sp3len=sp3.length;
         
-        int[] result=new int[3];
+        int[] result= {0,0,0};
         for(int i=0; i<answers.length; i++){
-            if(sp1[i%sp1len]==i)result[0]++;
-            if(sp2[i%sp2len]==i)result[1]++;
-            if(sp3[i%sp3len]==i)result[2]++;
+            if(sp1[i%sp1len]==answers[i])result[0]++;
+            if(sp2[i%sp2len]==answers[i])result[1]++;
+            if(sp3[i%sp3len]==answers[i])result[2]++;
         }
         int[] index={1,2,3};
         int max=0,cnt=0;
         for(int i=0; i<3; i++){
-            if(result[i]>max){
-                max=result[i];
-                cnt++;
-            }
+            if(result[i]>=max) max=result[i];
         }
         for(int i=0; i<3; i++){
-            if(result[i]!=max) index[i]=0;
+            if(result[i]==max) cnt++;
+            else index[i]=-1;
         }
         int[] answer=new int[cnt];
         int j=0;
-        for(int i=0; i<3; i++){
-            if(index[i]==0)continue;
-            answer[j]=i;
-            j++;
+        for(int i=0; j<cnt; i++){
+            if(index[i]==-1)continue;
+            else {
+                answer[j]=index[i];
+                j++;
+            }
         }
+        Arrays.sort(answer);
         return answer;
     }
 }
