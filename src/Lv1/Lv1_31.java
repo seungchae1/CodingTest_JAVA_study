6615d8444a86732c36d34f56973a5bf9514009de
seungchae <s2125@e-mirim.hs.kr>
2022-04-19 18:37:09 +0900

    문자열 다루기 기본

---
+++ src/Lv1/Lv1_31.java
@@ -0,0 +1,12 @@
+package Lv1;
+
+public class Lv1_31 {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(s.length()!=4 && s.length()!=6) return false;
+        for(int i=0; i<s.length(); i++){
+            if('0'<s.charAt(i) && s.charAt(i)>'9') return false;
+        }
+        return answer;
+    }
+}
