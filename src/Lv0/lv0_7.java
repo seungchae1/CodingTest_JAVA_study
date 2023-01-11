cb984d07c32d9444f82e683cd5634c096f5c36f7
seungchae <s2125@e-mirim.hs.kr>
2023-01-11 23:21:05 +0900

    피자 나눠먹기(1)

---
+++ src/Lv0/lv0_7.java
@@ -0,0 +1,11 @@
+package Lv0;
+// ÇÇÀÚ ³ª´²¸Ô±â(1)
+public class lv0_7 {
+	class Solution {
+	    public int solution(int n) {
+	        int answer = n/7;
+	        if(n>answer*7) answer++;
+	        return answer;
+	    }
+	}
+}
